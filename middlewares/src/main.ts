a3e8c03149dc05059c39269fb0a4af1532cb5a0c
CapibaraAnonimo <adrianarnaizcano@gmail.com>
2022-11-15 18:43:30 +0100

    hecho el ejercicio de validaciones

--- middlewares/src/main.ts
+++ middlewares/src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { loggerMiddleware } from './middlewares/logger.middleware';
 import { GeneralExeptionFilter } from './filters/exceptions/general-exeption/general-exeption.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(loggerMiddleware);
   app.useGlobalFilters(new GeneralExeptionFilter());
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 
--- middlewares/src/users/dto/create-user.dto.ts
+++ middlewares/src/users/dto/create-user.dto.ts
@@ -1,5 +1,41 @@
+import {
+  IsAlpha,
+  IsBoolean,
+  IsEmail,
+  IsEnum,
+  IsInt,
+  IsOptional,
+  IsString,
+  Max,
+  Min,
+} from 'class-validator';
+
+enum userTypes {
+  'admin',
+  'client',
+}
+
 export class CreateUserDto {
+  @IsAlpha('es-ES', {
+    message: 'El nombre solo debe tener letras',
+  })
   name: string;
+
+  @IsString()
+  @IsOptional()
   address: string;
+
+  @IsInt()
+  @Min(18)
+  @Max(90)
   age: number;
+
+  @IsEmail()
+  email: string;
+
+  @IsBoolean()
+  single: boolean;
+
+  @IsEnum(userTypes)
+  userType: string;
 }
